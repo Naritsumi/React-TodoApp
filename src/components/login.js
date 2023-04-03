6dbf5f99a367e7aac797c34c14ec309788d1e523
Naritsumi <haridian6bnaza09@gmail.com>
2023-04-03 15:54:41 +0100

    Init

---
+++ src/components/login.js
@@ -0,0 +1,17 @@
+export default function login() {
+    return (  
+    <div className="login">
+        <h1>Todo List</h1>
+        <div className="login-container">
+            <input type="email" 
+                //onChange={handleEmailChange} 
+                //value={email} 
+                />
+            <input type="password"
+                />
+            <button>Sign in</button>
+        </div>
+    </div>
+    )
+}
+
